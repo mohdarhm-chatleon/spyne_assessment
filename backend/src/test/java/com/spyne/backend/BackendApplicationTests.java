a8b24941e3babdaac5c9b91dcbcfc2b7c88dd53c
Shah Mohd Arhum <shahmohd.arhum@chateleon.com>
2024-11-15 10:09:04 +0530

    idk initial commit i guess

---
+++ backend/src/test/java/com/spyne/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spyne.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
